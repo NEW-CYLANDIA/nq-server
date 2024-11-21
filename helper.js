fc8c8853cad8ec7491ab11b4c730934b9ee26275
izzy kestrel <36773942+iznaut@users.noreply.github.com>
2024-11-21 12:14:55 -0600

    generate QR codes on db init

--- helper.js
+++ helper.js
@@ -188,6 +188,13 @@ exports.dbSyncDreamTable = async () => {
                         testEntryCreated = true
                     }
 
+                    var QRCode = require('qrcode')
+
+                    QRCode.toFile(`qr/${url_part}.png`, `https://dreamscape-explorer.app/bridges/${url_part}.html`, function (err) {
+                        if (err) throw err
+                        console.log('done')
+                    })
+
                     await db.query('insert into dreams (url_part, creator_id, title, chat_message) values ($1, $2, $3, $4)', [url_part, authorIds.indexOf(author) + 1, title, "this is a placeholder message! replace me! @~@"])
                 } catch (err) {
                     console.error(err);
